6f01b8abbe98678ff4d33ef02a1bf8f18643a001
pacalpha <jaylwilso@outlook.com>
2025-01-21 23:48:03 -0500

    Revised delete castmember function

--- eMS.cpp
+++ eMS.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
-#include <algorithm>
+#include <algorithm> //std::remove_if
+#include <set>
 
 class Person
 {
@@ -105,10 +106,9 @@ public:
     CastMember(std::string n, int id) : Employee(n, id) {}
 };
 
-std::vector<CastMember> castmembers =
-    {
-        CastMember("j", 1),
-        CastMember("jay", 2)};
+std::vector<CastMember> castmembers;
+std::set<int> usedIds;
+int nextId = 1;
 
 class Manager : public Employee
 {
@@ -126,6 +126,10 @@ public:
                       << "\t";
             member.checkClockStatus();
         }
+        if (castmembers.empty())
+        {
+            std::cout << "No Castmembers listed yet\n";
+        }
     }
 
     void createCastMember(std::vector<CastMember> &castmembers)
@@ -134,11 +138,17 @@ public:
         std::string castName;
         std::getline(std::cin >> std::ws, castName);
 
-        int castNum = castmembers.size() + 1;
+        while (usedIds.find(nextId) != usedIds.end())
+        {
+            nextId++; // increment until a free Id is found
+        }
 
-        castmembers.push_back(CastMember(castName, castNum));
+        castmembers.push_back(CastMember(castName, nextId));
+        usedIds.insert(nextId); // add new id to the set of IDs
 
-        std::cout << getName() << " has successfully added " << castName << "with ID: " << castNum << "!\n";
+        std::cout << getName() << " has successfully added " << castName << " with ID: " << nextId << "!\n";
+
+        nextId++;
     }
     void deleteCastMember(std::vector<CastMember> &castmembers, int id)
     {
@@ -146,6 +156,7 @@ public:
                                  { return member.getEmployeeId() == id; });
         if (it != castmembers.end())
         {
+            usedIds.erase(id);
             castmembers.erase(it, castmembers.end());
             std::cout << getName() << " ID: " << id << " has been successfully deleted.\n";
         }
@@ -203,7 +214,6 @@ employeeLogIn:
                 std::cout << "Please select an option: ";
 
                 int castMenu = validateInput();
-                // std::cin >> castMenu;
 
                 switch (castMenu)
                 {
@@ -234,63 +244,64 @@ employeeLogIn:
                     break;
                 }
             }
-            for (Manager &mg : managers)
+        }
+        for (Manager &mg : managers)
+        {
+            if (employeeNum == mg.getEmployeeId())
             {
-                if (employeeNum == mg.getEmployeeId())
+                found = true;
+                std::cout << "welcome " << mg.getName() << "!\n";
+                std::cout << "1. Check clock status\n";
+                std::cout << "2. Change clock staus\n";
+                std::cout << "3. Clock History\n";
+                std::cout << "4. List all castmembers\n";
+                std::cout << "5. Create cast member\n";
+                std::cout << "6. Delete Castmember\n";
+                std::cout << "7. EXIT\n";
+                std::cout << "Please select an option: ";
+
+                int managerMenu = validateInput();
+
+                switch (managerMenu)
                 {
-                    found = true;
-                    std::cout << "welcome " << mg.getName() << "!\n";
-                    std::cout << "1. Check clock status\n";
-                    std::cout << "2. Change clock staus\n";
-                    std::cout << "3. Clock History\n";
-                    std::cout << "4. List all castmembers\n";
-                    std::cout << "5. Create cast member\n";
-                    std::cout << "6. Delete Castmember\n";
-                    std::cout << "7. EXIT\n";
-                    std::cout << "Please select an option: ";
-
-                    int managerMenu = validateInput();
-
-                    switch (managerMenu)
-                    {
-                    case 1:
-                        system("clear");
-                        mg.checkClockStatus();
-                        break;
-                    case 2:
-                        system("clear");
-                        mg.changeClockStatus();
-                        break;
-                    case 3:
-                        system("clear");
-                        mg.displayClockHistory();
-                        break;
-                    case 4:
-                        system("clear");
-                        mg.listAllCastMembers(castmembers);
-                        break;
-                    case 5:
-                        system("clear");
-                        mg.createCastMember(castmembers);
-                        break;
-                    case 6:
-                        std::cout << "Enter ID for cast member to delete: ";
-                        mg.deleteCastMember(castmembers, validateInput());
-                        break;
-                    case 7:
-                        system("clear");
-                        goto employeeLogIn;
-                        break;
-                    default:
-                        std::cout << "Invalid input\n";
-                    }
+                case 1:
+                    system("clear");
+                    mg.checkClockStatus();
+                    break;
+                case 2:
+                    system("clear");
+                    mg.changeClockStatus();
+                    break;
+                case 3:
+                    system("clear");
+                    mg.displayClockHistory();
+                    break;
+                case 4:
+                    system("clear");
+                    mg.listAllCastMembers(castmembers);
+                    break;
+                case 5:
+                    system("clear");
+                    mg.createCastMember(castmembers);
+                    break;
+                case 6:
+                    system("clear");
+                    std::cout << "Enter ID for cast member to delete: ";
+                    mg.deleteCastMember(castmembers, validateInput());
+                    break;
+                case 7:
+                    system("clear");
+                    goto employeeLogIn;
+                    break;
+                default:
+                    std::cout << "Invalid input\n";
                 }
             }
-            if (!found)
-            {
-                std::cout << "Invalid input.Please try again\n";
-                goto employeeLogIn;
-            }
+        }
+        if (!found)
+        {
+            std::cout << "Invalid input.Please try again\n";
+            goto employeeLogIn;
         }
     }
 }
